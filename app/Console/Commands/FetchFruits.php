d836e93989bd06764e4503b91db8ad65d21ea631
muhemmad-1303 <muhammedb@mozilor.com>
2023-09-21 10:21:11 +0530

    email notification

--- app/Console/Commands/FetchFruits.php
+++ app/Console/Commands/FetchFruits.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Models\Fruit;
+use App\Models\User;
+use App\Notifications\FruitsImported;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
@@ -29,25 +31,27 @@ class FetchFruits extends Command
     {
         //
         $response = Http::get('https://fruityvice.com/api/fruit/all');
-            if ($response->successful()) {
-                $fruitsData = $response->json();
-                foreach ($fruitsData as $fruitData) {
-                    Fruit::create([
-                        'name' => $fruitData['name'],
-                        'family' => $fruitData['family'],
-                        'order' => $fruitData['order'],
-                        'genus' => $fruitData['genus'],
-                        'calories' => $fruitData['nutritions']['calories'],
-                        'fat' => $fruitData['nutritions']['fat'],
-                        'sugar' => $fruitData['nutritions']['sugar'],
-                        'carbohydrates' => $fruitData['nutritions']['carbohydrates'],
-                        'protein' => $fruitData['nutritions']['protein'],
-                    ]);
-                }
-
-                $this->info('Fruits data has been fetched and saved to the database.');
-            } else {
-                $this->error('Failed to fetch fruits data from the API.');
+        if ($response->successful()) {
+            $fruitsData = $response->json();
+            foreach ($fruitsData as $fruitData) {
+                Fruit::create([
+                    'name' => $fruitData['name'],
+                    'family' => $fruitData['family'],
+                    'order' => $fruitData['order'],
+                    'genus' => $fruitData['genus'],
+                    'calories' => $fruitData['nutritions']['calories'],
+                    'fat' => $fruitData['nutritions']['fat'],
+                    'sugar' => $fruitData['nutritions']['sugar'],
+                    'carbohydrates' => $fruitData['nutritions']['carbohydrates'],
+                    'protein' => $fruitData['nutritions']['protein'],
+                ]);
             }
+            $adminUser = User::where('role', 'admin')->first(); // Replace with your actual logic to find the admin user
+            $adminUser->notify(new FruitsImported());
+
+            $this->info('Fruits data has been fetched and saved to the database.');
+        } else {
+            $this->error('Failed to fetch fruits data from the API.');
+        }
     }
 }
